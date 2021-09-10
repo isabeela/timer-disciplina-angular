9d5add11a9b1910057542d9ba1ec9f8f8719e140
isabeela <isabeelabg@gmail.com>
2021-09-10 17:25:06 -0300

    2 files modified, 7 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,24 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from './navbar/navbar.component';
+
+import { DisciplinasComponent } from './disciplinas/disciplinas.component';
+
+import { TempoService } from '../tempo.service';
+
+import { DisciplinaService } from '../disciplina.service';
 
 @NgModule({
   imports: [BrowserModule, FormsModule],
-  declarations: [AppComponent, HelloComponent, HomeComponent],
-  bootstrap: [AppComponent]
+  declarations: [
+    AppComponent,
+    HelloComponent,
+    HomeComponent,
+    NavbarComponent,
+    DisciplinasComponent
+  ],
+  bootstrap: [AppComponent],
+  providers: [TempoService, DisciplinaService]
 })
 export class AppModule {}
